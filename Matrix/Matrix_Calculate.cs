d1f00194d87d5a27acfcf5f669a00b74a57ad350
gorohhovv <andrej.gorokhoff2017@yandex.ru>
2021-12-25 14:59:45 +0300

    Правки приложения

--- Matrix/Matrix_Calculate.cs
+++ Matrix/Matrix_Calculate.cs
@@ -8,43 +8,69 @@ namespace Matrix
 {
     public class Matrix_Calculate
     {
-        private readonly int[,] _Matrix;
-        IReadMatrix _readMatrix;
+        private readonly int[,] _Matrix; //Матрица, сохраненная в виде двумерного массива целых чисел
+        IReadMatrix _readMatrix; //Интерфейс для считывания данных(может хранится ссылка на объект одного из 3 классов)
 
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="readMatrix"></param>
         public Matrix_Calculate(IReadMatrix readMatrix)
         {
-            _readMatrix = readMatrix;
-            _Matrix = readMatrix.Read();
+            _readMatrix = readMatrix; //Получение ссылки на класс - поставщик данных
+            _Matrix = readMatrix.Read(); //Считывание данных
         }
 
-        public int FindWay(int v1, int v2, int count)
+        /// <summary>
+        /// Метод нахождения пути между двумя вершинами
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        public int FindWay(int v1, int v2)
         {
-            int[,] Current_Matrix = Copy_Matrix(_Matrix);
-            int way = 1;
+            int count_vertex = _Matrix.GetLength(0); //Кол-во вершин графа
+            int[,] Current_Matrix = Copy_Matrix(_Matrix); //Копируем значение исходной матрицы
+            int way = 1; //Текущая степень матрицы
             while (Current_Matrix[v1-1, v2-1] == 0)
             {
-                Current_Matrix = PowMatrix(Current_Matrix, _Matrix);
+                Current_Matrix = PowMatrix(Current_Matrix, _Matrix); //Возведение матрицы в следующую степень
 
                 way++;
 
-                if (way == count)
+                if (way == count_vertex) //Случай, когда вершины не имеют связей
                     return -1;
             }
 
-            return way;
+            return way; //Возвращение пути между двумя вершинами
         }
 
+        /// <summary>
+        /// Нахождение колличества маршрутов
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <param name="way"></param>
+        /// <returns></returns>
         public int FindCount(int v1, int v2, int way)
         {
-            int[,] Current_Matrix = Copy_Matrix(_Matrix);
-            for (int i = 1; i < way; i++)
+            int[,] Current_Matrix = Copy_Matrix(_Matrix); //Копируем значение исходной матрицы
+            for (int i = 1; i < way; i++) //Возведение исходной матрицы в степень way
             {
                 Current_Matrix = PowMatrix(Current_Matrix, _Matrix);
             }
 
-            return Current_Matrix[v1 - 1, v2 - 1];
+            return Current_Matrix[v1 - 1, v2 - 1]; //пересечение вершин в матрице- количество маршрутов длины, равной степени матрицы
         }
 
+        /// <summary>
+        /// Возведение матрицы в степень
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
         private int[,] PowMatrix(int[,] a, int[,] b)
         {
             if (a.GetLength(1) != b.GetLength(0)) throw new Exception("Матрицы нельзя перемножить");
@@ -62,6 +88,11 @@ namespace Matrix
             return r;
         }
 
+        /// <summary>
+        /// Копирование матрицы по значению
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <returns></returns>
         private int[,] Copy_Matrix(int[,] matrix)
         {
             int[,] Current_Matrix = new int[matrix.GetLength(0), matrix.GetLength(1)];
@@ -72,7 +103,6 @@ namespace Matrix
                     Current_Matrix[i, j] = matrix[i, j];
                 }
             }
-
             return Current_Matrix;
         }
     }
--- Matrix/Program.cs
+++ Matrix/Program.cs
@@ -1,28 +1,28 @@
 ﻿using System;
-using System.IO;
-
 
 namespace Matrix
 {
     public class Program
     {
         static void Main(string[] args)
-        {
-            //Кол - во вершин - 7
-            string ConStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\andre\source\repos\Matrix\Matrix\MatrixDatabase.mdf;Integrated Security=True";
+        {          
             Text_File text_File = new Text_File(@"C:\Users\andre\source\repos\Matrix\Matrix\file_matrix.txt");
             Matrix_Calculate matrix_Calculate = new Matrix_Calculate(text_File);
-            Console.WriteLine(matrix_Calculate.FindWay(2, 7, 7));
+            Console.WriteLine("Текстовый файл:");
+            Console.WriteLine($"Расстояние между v1 и v7 = {matrix_Calculate.FindWay(1, 7)}"); //Нахождение растояние между вершинами v1 и v7
+            Console.WriteLine($"Кол-во путей длины(3) между v4 и v6 = {matrix_Calculate.FindCount(4, 6, 3)}\n"); //Нахождение кол-ва путей длины(3) между вершинами v4 и v6
 
+            Console.WriteLine("Excel файл:");
             Excel_File excel_File = new Excel_File(@"C:\Users\andre\source\repos\Matrix\Matrix\work2.xlsx");
             Matrix_Calculate matrix_Calculate1 = new Matrix_Calculate(excel_File);
-            Console.WriteLine(matrix_Calculate1.FindWay(2, 7, 7));
+            Console.WriteLine($"Расстояние между v1 и v7 = {matrix_Calculate1.FindWay(1, 7)}");
+            Console.WriteLine($"Кол-во путей длины(3) между v4 и v6 = {matrix_Calculate1.FindCount(4, 6, 3)}\n");
 
-            Database_File database_File = new Database_File(ConStr);
+            Console.WriteLine("База данных:");
+            Database_File database_File = new Database_File(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\andre\source\repos\Matrix\Matrix\MatrixDatabase.mdf;Integrated Security=True");
             Matrix_Calculate matrix_Calculate2 = new Matrix_Calculate(database_File);
-            Console.WriteLine(matrix_Calculate2.FindWay(2, 7, 7));
-
-
+            Console.WriteLine($"Расстояние между v1 и v7 = {matrix_Calculate2.FindWay(1, 7)}");
+            Console.WriteLine($"Кол-во путей длины(3) между v4 и v6 = {matrix_Calculate2.FindCount(4, 6, 3)}\n");
 
         }
     }
